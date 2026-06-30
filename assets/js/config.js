a6c525456fce884357e8e799a3663953e0a78f7d
gostevskaya <93514306+gostevskaya@users.noreply.github.com>
2026-06-30 22:03:42 +0300

    Add files via upload

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,6 +2,5 @@ window.SITE_CONFIG = {
   phoneDisplay: "+38 075 45 075 90",
   phoneLink: "+380754507590",
   telegram: "https://t.me/olena_keystoself",
-  telegramChannel: "https://t.me/keystoself_channel",
   viber: "viber://chat?number=%2B380754507590"
 };
